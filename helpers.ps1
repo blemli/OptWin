4583871686299ccdbbf22db5efade1b629d9f48a
grafst <grafst@users.noreply.github.com>
2023-11-30 11:58:22 +0100

    install-chocolatey, activesetup, approve-execution

--- helpers.ps1
+++ helpers.ps1
@@ -3,6 +3,19 @@ Function Uninstall-Helpers {
     uninstall-Chocolatey
     #TODO: also remove tempdir
 }
+Function Install-Chocolatey {
+    $ChocoPath="C:\ProgramData\chocolatey"
+    if(Test-Path $ChocoPath){
+        Remove-Item $ChocoPath -Force -Recurse
+    }
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    RefreshEnv.cmd
+    Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+    RefreshEnv.cmd
+    choco feature enable -n=allowGlobalConfirmation
+}
+
 
 Function Set-Dword{
     param(
@@ -187,4 +200,84 @@ Function Read-Password() {
         }
     }
     return $first
+}
+
+
+function Add-ActiveSetupComponent{
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $DisplayName,
+        [Parameter(Mandatory=$True)]
+        [String]
+        $Id,
+        [Parameter()]
+        [String]
+        $Locale,
+        [Parameter(Mandatory=$True)]
+        [String]
+        $Script,
+        [Parameter()]
+        [String]
+        $Version,
+        [Parameter()]
+        [String]
+        [ValidateSet($null,"First","Last")]
+        $Position = $null
+    )
+
+    switch ($Position) {
+        $null  { $PositionChar="" }
+        "First" { $PositionChar="<" }
+        "Last"  { $PositionChar=">" }
+        Default {$PositionChar=""}
+    }
+
+    $BasePath="HKLM:\Software\Microsoft\Active Setup\Installed Components"
+    $RegPath= Join-Path $BasePath ("$PositionChar"+"$Id")
+    New-Item -Path $RegPath
+    
+    New-ItemProperty -Path $RegPath -name "(default)" -value $DisplayName -Force
+    if($Locale){
+        New-ItemProperty -Path $RegPath -Name "Locale" -value "de"
+    }
+    New-ItemProperty -Path $RegPath -Name "StubPath" -value "powershell.exe -WindowStyle Hidden -NonInteractive -Command Set-ExecutionPolicy Bypass -Scope Process; $script" -Force
+    if($Version){
+        New-ItemProperty -Path $RegPath -Name "Version" -value $Version -Force
+    }
+}
+
+function Disable-ActiveSetupComponent{
+    param (
+        [Parameter(Mandatory=$True)]
+        [String]
+        $Id
+    )
+    #todo: also try <$id and >$id
+    $BasePath="HKLM:\Software\Microsoft\Active Setup\Installed Components"
+    $RegPath= Join-Path $BasePath $Id
+    New-ItemProperty -Path $RegPath -Name IsInstalled -value 0 -Force
+}
+
+function Enable-ActiveSetupComponent{
+    param (
+        [Parameter(Mandatory=$True)]
+        [String]
+        $Id
+    )
+    #todo: also try <$id and >$id
+    $BasePath="HKLM:\Software\Microsoft\Active Setup\Installed Components"
+    $RegPath= Join-Path $BasePath $Id
+    New-ItemProperty -Path $RegPath -Name IsInstalled -value 1 -Force
+}
+
+Function Approve-ScriptExecution{
+    $ExecutionPolicy = Get-ExecutionPolicy -Scope Process
+    if($ExecutionPolicy -eq "RemoteSigned" -or $ExecutionPolicy -eq "Unrestricted" -or $ExecutionPolicy -eq "Bypass") {
+        Write-Host "ScriptExecution is already allowed ($ExecutionPolicy)"
+    } else {
+        Write-Host "ExecutionPolicy is $ExecutionPolicy, changing to RemoteSigned"
+        Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+    }
 }

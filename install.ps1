1b6f8cd631ccd4534deba19f24be5c314aca1d18
grafst <grafst@users.noreply.github.com>
2024-01-04 18:13:27 +0100

    add nugetprovider to install script

--- install.ps1
+++ install.ps1
@@ -1,4 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Get-PSRepository -Name PSGallery | Select-Object -Expandproperty InstallationPolicy | Set-Variable -Name OldInstallationPolicy
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name OptWin -Force -Scope AllUsers
